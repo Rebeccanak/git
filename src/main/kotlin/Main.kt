0d38a0a391cbe4b1c366a7964533e2d5cc4c94ad
Rebeccanak <rebeccanakachwaaa@gmail.com>
2023-03-09 08:58:14 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,58 @@
+fun main() {
+println(getnum("home is good","hoe or not"))
+//    Write and invoke a function that takes in any 2 strings and returns the first
+//    character of the longer string or the first character of the first string if they are
+//            equal in length.
+println(getint(19,20,13))
+
+}
+fun getnum(name1:String,name2:String):String {
+    var num = ("Home sweet home")
+    if(num==name1)
+        println("Home")
+       word("potatoes")
+
+
+       else{
+println("sweet")
+return num
+//    var num1 = "I would love to understand kotlin more"
+//    var num2 = "It will help me later"
+//    if (num1 == num2)
+//        println("I")
+//
+//  else{
+//      println("It")
+//        return getnum
+//
+    }
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+fun getint(num1:Int,num2:Int,num3:Int):Int{
+    var names = arrayOf(num1,num2,num3)
+    return names.
+}
+//Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output. (3 pts)
+fun word(name:String){
+    var word = "Potatoes"
+    println(word.split("potatoe")
+
+
+
+
+    }
+
+//    Create a calculator class that is capable of the following functions:
+//    (i) Addition of any 2 numbers
+//            (ii) Subtraction between any 2 numbers
+//            (iii) Accurate division between any 2 numbers
+//    (iv) Multiplication of any 2 numbers
+//            Instantiate your calculator object and invoke all its functions
+
+class Calculator(var addition:Int, var substraction:Int, var accurate:Int, var multilplication:Int){
+var hat = Calculator(20,24,50,10)
+}
+
+
+
